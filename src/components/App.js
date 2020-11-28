8017840d7f6b0f4f1963ed88f042397a613a233c
sl24 <slvitalik24@gmail.com>
2020-11-28 18:34:37 +0200

    Add Container

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,33 @@
+import Container from './Container/Container';
 
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendsList/FriendList';
+import Transactions from './Transactions/Transactions';
 
-import Profile from './Profile/Profile'
-import Statistics from './Statistics/Statistics'
-import FriendList from './FriendsList/FriendList'
-import Transactions from './Transactions/Transactions'
-
-import userData from '../data/user.json'
-import statsData from '../data/statistics.json'
-import friendsData from '../data/friends.json'
-import transactionsData from '../data/transactions.json'
+import userData from '../data/user.json';
+import statsData from '../data/statistics.json';
+import friendsData from '../data/friends.json';
+import transactionsData from '../data/transactions.json';
 
 const App = () => {
-    return (
-        <>
-            <Profile
-                name={userData.name}
-                tag={userData.tag}
-                location={userData.location}
-                avatar={userData.avatar}
-                stats={userData.stats} />
-            
-            <Statistics title="Upload stats" stats={statsData} />
-            
-            <FriendList friends={friendsData} />
+  return (
+    <Container>
+      <Profile
+        name={userData.name}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
 
-            <Transactions items={transactionsData} />
-        </>
-    )
+      <Statistics title="Upload stats" stats={statsData} />
+
+      <FriendList friends={friendsData} />
+
+      <Transactions items={transactionsData} />
+    </Container>
+  );
 };
 
-export default App
+export default App;
