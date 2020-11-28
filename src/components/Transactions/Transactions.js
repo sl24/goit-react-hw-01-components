79bab74f04e13fd960996a8df60dc1f4130c09d2
sl24 <slvitalik24@gmail.com>
2020-11-28 20:48:11 +0200

    Add styles for Trnsactions

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,40 +1,42 @@
 import PropTypes from 'prop-types';
 
+import s from './Transactions.module.css';
+
 const Transactions = ({ items }) => {
-    if (items.length === 0) return null
+  if (items.length === 0) return null;
 
-    return (
-        <table class="transactionHistory">
-            <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
-                </tr>
-            </thead>
+  return (
+    <table className={s.transactionHistory}>
+      <thead className={s.thead}>
+        <tr>
+          <th className={s.tableData}>Type</th>
+          <th className={s.tableData}>Amount</th>
+          <th className={s.tableData}>Currency</th>
+        </tr>
+      </thead>
 
-            <tbody>{
-                items.map(item => (
-                <tr key={item.id}>
-                    <td>{item.type}</td>
-                    <td>{item.amount}</td>
-                    <td>{item.currency}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
-    )
-}
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id} className={s.row}>
+            <td className={s.tableData}>{item.type}</td>
+            <td className={s.tableData}>{item.amount}</td>
+            <td className={s.tableData}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 Transactions.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-            amount: PropTypes.string.isRequired,
-            currency: PropTypes.string.isRequired,
-        }),
-    ),
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ),
 };
 
 export default Transactions;
